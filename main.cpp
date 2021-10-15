21989ca64e892e083d7e738fd17b98cc2518ddb2
linares <toninolg@hotmail.com>
2021-10-15 10:32:06 +0200

    ImGui

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@
 #include "shader.h"
 #include "texture.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 
 static void DebugMessageCallBack(GLenum source, GLenum type, GLenum id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
 {
@@ -92,48 +96,71 @@ int main(void)
 
     glm::mat4 proj  = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
     glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0 , 0));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200 , 0));
-
-    glm::mat4 mvp  = proj * view * model;
 
     // Create shaders
     Shader shader("../ChernoOpenGL/assets/shaders/Basic.shader");
     shader.bind();
-    shader.setUniform4f("u_color", 0.8f, 0.3f, 0.8f, 1.0f);
 
     Texture texture("../ChernoOpenGL/assets/textures/cherno.png");
     texture.bind();
-    shader.setUniform1i("u_texture", 0);
-    shader.setUniformMat4f("u_MVP", mvp);
+    shader.setUniform1i("u_texture", 0);;
 
     va.unbind();
     vb.unbind();
     ib.unbind();
     shader.unbind();
 
+    Renderer renderer;
+
+    // ********** ImGui **************************
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+    ImGui::StyleColorsDark();
+    // *******************************************
+
+
+    glm::vec3 translation(200.0f, 200.0f , 0.0f);
+
     float r = 0.0f;
     float increment = 0.05f;
-
-    Renderer renderer;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         renderer.clear();
 
+        // **********Start the Dear ImGui frame ********************************************************************************************
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        // *********************************************************************************************************************************
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 mvp  = proj * view * model;
+
         shader.bind();
         shader.setUniform4f("u_color", r, 0.3f, 0.8f, 1.0f);
+        shader.setUniformMat4f("u_MVP", mvp);
 
         // Issue a draw call on the buffer that is bound
         renderer.draw(va, ib, shader);
 
-        if (r > 1.0f){
+        if (r > 1.0f)
             increment = -0.05f;
-        }
-        else if (r < 0.0f){
+        else if (r < 0.0f)
             increment = 0.05f;
-        }
+
         r += increment;
+
+        // ********** ImGui Render ******************
+        {
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        // ******************************************
 
         /* Swap front and back buffers */
         GLCall(glfwSwapBuffers(window));
@@ -142,6 +169,13 @@ int main(void)
         glfwPollEvents();
     }
 
+    // ********** ImGui CleanUp ******************
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    // *******************************************
+
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
