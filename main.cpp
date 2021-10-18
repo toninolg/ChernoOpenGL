ca0f6fe65408f50c637df248ee43c27932884a6b
linares <toninolg@hotmail.com>
2021-10-18 21:51:16 +0200

    Rendering multiple objects

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 600, "Cherno OpenGL", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "Cherno OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -60,8 +60,8 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Uncomment to output errors
-//    glEnable(GL_DEBUG_OUTPUT);
-//    glDebugMessageCallback(DebugMessageCallBack, nullptr);
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(DebugMessageCallBack, nullptr);
 
     // Print versions
     fprintf(stdout, "Status: Using OpenGL %s\n", glGetString(GL_VERSION)); fflush(stdout);
@@ -69,10 +69,10 @@ int main(void)
 
     // Triangle vertex positions
     float positions [] = {
-        100.0f, 100.0f, 0.0f, 0.0f, // 0
-        200.0f, 100.0f, 1.0f, 0.0f, // 1
-        200.0f, 200.0f, 1.0f, 1.0f, // 2
-        100.0f, 200.0f, 0.0f, 1.0f  // 3
+        -50.0f, -50.0f, 0.0f, 0.0f, // 0
+         50.0f, -50.0f, 1.0f, 0.0f, // 1
+         50.0f,  50.0f, 1.0f, 1.0f, // 2
+        -50.0f,  50.0f, 0.0f, 1.0f  // 3
     };
 
     unsigned int indices[] = {
@@ -95,7 +95,7 @@ int main(void)
     IndexBuffer ib (indices, 6);
 
     glm::mat4 proj  = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0 , 0));
+    glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0 , 0));
 
     // Create shaders
     Shader shader("../ChernoOpenGL/assets/shaders/Basic.shader");
@@ -120,7 +120,8 @@ int main(void)
     // *******************************************
 
 
-    glm::vec3 translation(200.0f, 200.0f , 0.0f);
+    glm::vec3 translationA(200.0f, 200.0f , 0.0f);
+    glm::vec3 translationB(400.0f, 200.0f , 0.0f);
 
     float r = 0.0f;
     float increment = 0.05f;
@@ -136,15 +137,21 @@ int main(void)
         ImGui::NewFrame();
         // *********************************************************************************************************************************
 
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 mvp  = proj * view * model;
-
         shader.bind();
-        shader.setUniform4f("u_color", r, 0.3f, 0.8f, 1.0f);
-        shader.setUniformMat4f("u_MVP", mvp);
 
-        // Issue a draw call on the buffer that is bound
-        renderer.draw(va, ib, shader);
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp  = proj * view * model;
+            shader.setUniformMat4f("u_MVP", mvp);
+            renderer.draw(va, ib, shader);
+        }
+
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp  = proj * view * model;
+            shader.setUniformMat4f("u_MVP", mvp);
+            renderer.draw(va, ib, shader);
+        }
 
         if (r > 1.0f)
             increment = -0.05f;
@@ -155,7 +162,8 @@ int main(void)
 
         // ********** ImGui Render ******************
         {
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         }
         ImGui::Render();
